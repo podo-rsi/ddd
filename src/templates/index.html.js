e017d76df44c06385449537cd07fa88f480592a2
Alexander Podolinski <podolinski@riege.com>
2025-12-02 18:56:04 +0100

    x

--- src/templates/index.html.js
+++ src/templates/index.html.js
@@ -42,7 +42,7 @@ function randomColor() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  return \`rgb(${r},${g},${b})\`;
+  return 'rgb(' + r + ',' + g + ',' + b + ')';
 }
 
 function blinkLetters() {
